b78fcd575cf9060ce79e5111d59a3a0fa268875e
arsen <arseniytitarenko93@yandex.ru>
2023-12-06 14:12:34 +0300

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+// Получаем все элементы с классом leader
+let leaders = document.querySelectorAll(".leader");
+
+// Для каждого элемента добавляем обработчик события click
+leaders.forEach(function(leader) {
+    leader.addEventListener("click", function() {
+        // Получаем элемент с классом leader-info внутри текущего элемента
+        let info = leader.querySelector(".leader-info");
+        // Переключаем видимость элемента с классом leader-info
+        info.classList.toggle("show");
+    });
+});
